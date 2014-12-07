04c456f3057a1937a569c11825ceed2a2cafa3ca
irbis1988 <irbis1988@gmail.com>
2014-12-07 20:06:24 +0300

    web client. widget states, widget conf lang, align

--- Client/WebClient/js/jquery.exolive.js
+++ Client/WebClient/js/jquery.exolive.js
@@ -8,11 +8,17 @@
 			onMessageReceive: function(msg){},
 			
 			align	:	'left',
-			invite	:	'Contact us',
-			lang	:	'ru'
-        };        
+			lang	:	'ru',
+			cultures:  {
+				ru :{
+					invite	:	'Свяжитесь с нами :)'
+				},
+				eng : {
+					invite	:	'Contact us :)'
+				}
+			}
+        };     	
 		var settings = $.extend({}, defaultOptions, options );
-		//var cultures = $.extend({}, defaultOptions, options );
 		var webSessionId = getUniqueCode();
 		var runtimeId = getUniqueCode();
 		var initState = false;
@@ -27,6 +33,8 @@
 		var lastReceiveMsgNum = 0;
 		var isReceiveProcess = false;
 		var fms = -1;
+		var widgetState = 1;
+		var widgetHtml = '';
 		
 		function getBrowserCaps(){
 			var caps = {};
@@ -102,10 +110,6 @@
 				createCookie("exolive", webSessionId, 30);
 			}
 			return getCookie("exolive");
-		}
-		
-		this.showData = function(){
-			alert(settings.apiKey);
 		}
 		
 		this.debugMessageSend = function(text){
@@ -210,40 +214,6 @@
 			receiveState = false;
 		}
 		
-		//var initInterval = setInterval(function(){
-			var webSession = ensureWebSession();
-			var caps = getBrowserCaps();
-			$.ajax({
-				type: "POST",
-				url: "http://localhost:7777/webclient/" + settings.apiKey + "/wsinit",
-				data: {
-					data: webSession,
-					caps: JSON.stringify(caps)
-				},
-				cache: false,
-				success: function (data, textStatus, jqXHR) {
-					if(initState) return;
-					if(data.ResultCode == 0){
-						//clearInterval(initInterval);
-						initState = true;
-						
-						
-						
-						
-						if(settings.onInitComplete != null){
-							settings.onInitComplete();
-						}
-					}				
-				},
-				error: function (jqXHR, textStatus, errorThrown) {					
-				}
-			});
-		//}, 1000);
-		
-		// sendMessage(100, "123");
-		// sendMessage(100, "uwioh rweughfeurhg uioehopirgh eoihjoi");
-		
-			
 		////////////////////////////////
 		function widgetGenerate(){
 			var wrapper_id='exolive',
@@ -251,21 +221,93 @@
 				 
 			function reposClose() {
 				if($(wrapper).position().left>=$('body').outerWidth()-$(wrapper).outerWidth())
-					$('#'+wrapper_id+'.active .close').css({'left':0,'marginRight':'-10px'})
+					$('#'+wrapper_id+'.active .close').removeClass('-right')
+					.addClass('-left');
 				if($(wrapper).position().left<=0)
-					$('#'+wrapper_id+'.active .close').css({'left':'100%','marginLeft':'-10px'})
+					$('#'+wrapper_id+'.active .close').removeClass('-left')
+					.addClass('-right');
 				if($(wrapper).position().top<=0)
-					$('#'+wrapper_id+'.active .close').css({'top':'100%','marginBottom':'-10px'})
+					$('#'+wrapper_id+'.active .close').removeClass('-top')
+					.addClass('-bottom');
 				if($(wrapper).position().top>=$('body').outerHeight()-$(wrapper).outerHeight())
-					$('#'+wrapper_id+'.active .close').css({'top':0,'marginTop':'-10px'})
+					$('#'+wrapper_id+'.active .close').removeClass('-bottom')
+					.addClass('-top');
 			}
 			
 			function toClose(){
 				$(wrapper+'.active').removeClass('active').addClass('minimise');
+				stateClassUpdate();
 				$(wrapper).draggable({disabled:true});
 			}
-				
-			$('body').append('<div id="'+wrapper_id+'" class="minimise"><div class="invite">'+settings.invite+'</div><span class="close">&times;</span><div class="cont"></div></div>');
+			
+			function stateClassUpdate(){
+				$(wrapper).removeClass('state-0').removeClass('state-1')
+					.removeClass('state-2').addClass('state-'+widgetState);
+			}
+			
+			function loadContent(state){
+				stateClassUpdate();
+				$(wrapper).find('.cont')
+					.html('<img class="loader" src="i/ajaxload.gif">');
+				$.ajax({
+					url: "widget_state_"+state+".html",
+					dataType: "html",
+					cache: false,
+					success: function(html){
+						
+						if(!widgetHtml)		
+							widgetHtml=html;
+						
+						$(wrapper).find('div.cont').html(html);
+						
+						$("form#lead-gen").submit(function() {
+							if( $('#email').val()=='' && $('#phone').val()=='' )
+								$('#email,#phone').addClass('error');
+							else{
+								$('#email,#phone').removeClass('error');
+								
+								widgetState = 2;//
+								loadContent(widgetState);//
+								
+							}
+							return false;
+						});
+						
+						$("form#chat").submit(function() {						
+							live.debugMessageSend($("#text").val());
+							return false;
+						});
+						
+					},
+					error: function(){
+						$(wrapper).find('div.cont').html('error');
+					},
+					complete:function(){
+						$(wrapper).draggable({disabled: false});
+						if($(wrapper).hasClass('active')){
+							$(wrapper+'.active').find('span.close')
+								.on('click',function(){ toClose(); });
+							$('body').keyup(function(e){ 
+								if(e.keyCode==27) toClose(); 
+							});
+						}
+					}
+				});
+			}
+			
+
+			/*function validateComments(input) {
+				//input.hide();
+				if (input.val().length < 20) {
+					input.setCustomValidity("Дайте более развернутый ответ.");   
+				}else {
+					// Длина комментария отвечает требованию, 
+					// поэтому очищаем сообщение об ошибке
+					input.setCustomValidity("");
+				}
+			}*/
+			
+			$('body').append('<div id="'+wrapper_id+'" class="minimise"><div class="invite">'+settings.cultures[settings.lang].invite+'</div><span class="close">&times;</span><div class="cont"></div></div>');
 			
 			$(wrapper).addClass('conf_'+settings.align)
 				.draggable({ 
@@ -285,44 +327,52 @@
 				if($('body').outerHeight() < $(wrapper).outerHeight()+$(wrapper).position().top)
 					$(wrapper).offset({top:$('body').outerHeight()-$(wrapper).outerHeight()});
 			});
-		
+			
 			if($(wrapper).hasClass('minimise')){
 				$(wrapper+'.minimise .invite').click(function(){
-							
+					
 					$(wrapper).removeClass('minimise').addClass('active');
-					$(wrapper).find('.cont').html('<img class="loader" src="i/ajaxload.gif">');
-							
-					$.ajax({
-						url: "widget.html",
-						dataType: "html",
-						cache: false,
-						success: function(html){
-							$(wrapper).find('div.cont').html(html);
-							$("#button").on('click',function() {
-							if($("#text").val())
-								live.debugMessageSend($("#text").val());
-							});
-						},
-						error: function(){
-							$(wrapper).find('div.cont').html('error');
-						},
-						complete:function(){
-							$(wrapper).draggable({disabled: false});
-							if($(wrapper).hasClass('active')){
-								$(wrapper+'.active').find('span.close').on('click',function(){ toClose(); });
-								$('body').keyup(function(e){ 
-									if(e.keyCode==27) toClose(); 
-								});
-							}
-						}
-					});			
+					
+					//widgetState = 1;
+						
+					loadContent(widgetState);
+					
 				});
 			}
-					
 		}
-		
-		widgetGenerate();
 		///////////////////////////////	
+		
+		//var initInterval = setInterval(function(){
+			var webSession = ensureWebSession();
+			var caps = getBrowserCaps();
+			$.ajax({
+				type: "POST",
+				url: "http://localhost:7777/webclient/" + settings.apiKey + "/wsinit",
+				data: {
+					data: webSession,
+					caps: JSON.stringify(caps)
+				},
+				cache: false,
+				success: function (data, textStatus, jqXHR) {
+					if(initState) return;
+					if(data.ResultCode == 0){
+						//clearInterval(initInterval);
+						initState = true;
+						
+						widgetGenerate();
+						
+						if(settings.onInitComplete != null){
+							settings.onInitComplete();
+						}
+					}				
+				},
+				error: function (jqXHR, textStatus, errorThrown) {					
+				}
+			});
+		//}, 1000);
+		
+		// sendMessage(100, "123");
+		// sendMessage(100, "uwioh rweughfeurhg uioehopirgh eoihjoi");
 		
 		startSending();
 		startReceiving();
